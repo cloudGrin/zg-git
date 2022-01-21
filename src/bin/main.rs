50accfaaca9279fa855d16e644542acd98dc6823
王文瀚 <nj.wangwenhan@gmail.com>
2022-01-21 17:15:11 +0800

    feat: 添加ci

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,22 +6,18 @@ fn main() -> CmdResult {
     // DEBUG
     // cmd_lib::set_debug(true);
     // init_builtin_logger();
-    let args = env::args().collect::<Vec<String>>();
-    let args_opt = args.get(1..);
-    if let Some(args) = args_opt {
-        let mut args = args.to_vec();
+    let mut args = env::args().collect::<Vec<String>>();
 
-        if args[0].eq("merge")
-            & !args.join(" ").contains("--ff")
-            & !args.join(" ").contains("--ff-only")
-            & !args.join(" ").contains("--no-ff")
-        {
-            println!("merge 默认使用 --no-ff");
-            args.push(String::from("--no-ff"));
-        }
-
-        let mut proc = spawn!(git $[args]).unwrap();
-        proc.wait().unwrap();
+    if args[0].eq("merge")
+        & !args.join(" ").contains("--ff")
+        & !args.join(" ").contains("--ff-only")
+        & !args.join(" ").contains("--no-ff")
+    {
+        println!("merge 默认使用 --no-ff");
+        args.push(String::from("--no-ff"));
     }
+
+    let mut proc = spawn!(git $[args]).unwrap();
+    proc.wait().unwrap();
     Ok(())
 }
